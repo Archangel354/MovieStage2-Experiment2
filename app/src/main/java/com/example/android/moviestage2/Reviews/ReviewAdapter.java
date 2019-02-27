e715e46af3af0b5a1781eb0622797e69a5ff606e
Archangel354 <rduncan354@gmail.com>
2019-02-27 15:03:57 -0500

    Created ReviewAdapter.java and ReviewUtils.java; added to ReviewsActivity.java,

--- app/src/main/java/com/example/android/moviestage2/Reviews/ReviewAdapter.java
+++ app/src/main/java/com/example/android/moviestage2/Reviews/ReviewAdapter.java
@@ -1,4 +1,20 @@
 package com.example.android.moviestage2.Reviews;
 
-public class ReviewAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder> {
+
+
+    public class ReviewViewHolder extends RecyclerView.ViewHolder {
+        public TextView rTextViewAuthor;
+        public TextView rTextViewContent;
+
+
+
+        public ReviewViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/example/android/moviestage2/Reviews/ReviewsActivity.java
+++ app/src/main/java/com/example/android/moviestage2/Reviews/ReviewsActivity.java
@@ -5,24 +5,54 @@ import android.content.Intent;
 import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.example.android.moviestage2.MovieList;
 import com.example.android.moviestage2.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ReviewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<ReviewList>> {
 
+    // Constant for logging
+    private static final String TAG = ReviewsActivity.class.getSimpleName();
+    private static final int REVIEWLIST_LOADER_ID = 2;
+
+    /** Adapter for the list of reviews from the JSON data */
+    private ReviewAdapter rAdapter;
+
+    // Find a reference to the in the layout
+    private RecyclerView rRecyclerView;
+    private ArrayList<ReviewList> rReviewList;
+
+    /** Query URL */
+    public String rUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviews);
+        rRecyclerView =  findViewById(R.id.recycler_reviewView);
 
-        Intent intent = getIntent();
-        Bundle reviewsUrlBundle = intent.getExtras();
+        // Create a new adapter that takes an empty list of movies as input
+        rAdapter = new ReviewAdapter(ReviewsActivity.this, rReviewList);
+        rRecyclerView.setHasFixedSize(true);
+        // Set the adapter on the {@link GridView} so the list can be populated in the user interface
+        rRecyclerView.setAdapter(rAdapter);
+
+
+
+
+
+
+
+
+//        Intent intent = getIntent();
+//        Bundle reviewsUrlBundle = intent.getExtras();
 //        TextView txtUrl = findViewById(R.id.txtMovieReviews);
 //
 //        if (reviewsUrlBundle != null){
